b0da2161b9a64948a9f3d05da4236a83b722cdea
Paige Struthers <pstruthers@unomaha.edu>
2025-08-11 19:06:31 -0500

    09: superglobals

---
+++ 09_superglobals.php
@@ -0,0 +1,20 @@
+<?php
+
+  /* ------- Superglobals ------- */
+  
+  // Built-in variables that are always available in all scopes
+
+  /*
+    $_GET - Contains information about variables passed through a URL or form
+    $_POST - Contains information about variables passed through a form
+    $_COOKIE - Contains information about variables passed through a cookie
+    $_SESSION - Contains information about variables passed through a session
+    $_SERVER - Contains information about the server environment
+    $_ENV - Contains information about the environment variables
+    $_FILES - Contains information about files uploaded to the script
+    $_REQUEST - Contains information about variables passed through the form or URL
+  */
+
+  var_dump($_SERVER);
+  echo '<br>' . '<br>';
+  echo $_SERVER['SERVER_NAME'];
