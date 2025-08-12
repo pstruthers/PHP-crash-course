4aacf0357bff0be6e0e691acfa6c06b1e4df5d56
Paige Struthers <pstruthers@unomaha.edu>
2025-08-11 19:24:06 -0500

    10: $_GET and $_POST superglobals

---
+++ 10_get_post.php
@@ -0,0 +1,24 @@
+<?php
+ 
+  /* ------- $_GET & $_POST Superglobals ------- */
+
+  // Pass data through URLs and forms using $_GET and $_POST superglobals
+  if(isset($_POST['submit'])) {
+    echo $_POST['name'];
+    echo $_POST['age'];
+  }
+?>
+
+<a href="<?php echo $_SERVER['PHP_SELF'] ?>?name=Paige&age=23">Click</a>
+
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+  <div>
+    <label for="name">Name: </label>
+    <input type="text" name="name">
+  </div>
+  <div>
+    <label for="age">Age: </label>
+    <input type="text" name="age">
+  </div>
+  <input type="submit" value="Submit" name="submit">
+</form>
